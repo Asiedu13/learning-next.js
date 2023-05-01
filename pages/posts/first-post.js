2e5183a1b22453692b6a5546fb2b8dba751e0300
prince <princekofasiedu@gmail.com>
2023-05-01 01:08:31 +0000

    Learnt about images and meta data handling

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,28 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import Head from 'next/head'
+import img from '../../public/images/profile.jpg'
 export default function firstPost() {
-    return <h1>First Post</h1>
+    return (
+        <>
+            <Head>
+                <title>My first Post</title>
+            </Head>
+            <h1>First Post</h1>
+
+            <h2>
+                <Link href='/'>Back to home</Link>
+            </h2>
+            
+           <Image 
+                src={img}
+                width={300}
+                height={300}
+                alt={'User profile image'}
+                placeholder={'blur'}
+                blurDataURL=''
+           />
+        
+        </>
+    )
 }
